57b4bb79dc3167983bc676232d9aaf4232edc482
msh <83120007+johntanas@users.noreply.github.com>
2022-04-03 23:21:03 +0800

    Update parse_price.py

--- parse_price.py
+++ parse_price.py
@@ -1,1 +1,4 @@
-
+def parse_price(price: str):
+    pattern = '\D?(\d*)(,*)?(\d+)(\.)(\d+)'
+    tokens = re.search(pattern, price, re.UNICODE)
+    decimal_str = tokens.group(1)+tokens.group(3)+"."+tokens.group(5)
